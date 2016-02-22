7a9e2e56e9eac08eac17c7effbb315f8c108add3
U-ATHERTON\msorentiny <msorentiny@MSORENTINY-WORK.corp.netledger.com>
2016-02-22 14:45:38 +0100

    Changed to PageAction instead of BrowserAction.

--- Chrome/src/ns_login_skip.js
+++ Chrome/src/ns_login_skip.js
@@ -1,17 +1,31 @@
-function skipNetSuiteLoginInCurrentTab(currentTab) {
+function skipNetSuiteLoginInCurrentTab(currentTab)
+{
     var currentUrl = currentTab.url;
 
     console.assert(typeof currentUrl == 'string', 'tab.url should be a string');
-	console.log("Updating URL");
+    console.log("Updating URL");
 
     var decodedUrl = decodeURIComponent(currentUrl);
     var newUrl = removeLoginPartFromDecodedUrl(decodedUrl);
-	chrome.tabs.update(currentTab.id, {url: newUrl});
+    chrome.tabs.update(currentTab.id, {url: newUrl});
 }
 
-function removeLoginPartFromDecodedUrl(url) {
-    var urlWithoutLogin = url.replace("/pages/login.jsp?rdt=", "");
-    return urlWithoutLogin;
+function removeLoginPartFromDecodedUrl(url)
+{
+    return url.replace("/pages/login.jsp?rdt=", "");
 }
 
-chrome.browserAction.onClicked.addListener(function(tab) {skipNetSuiteLoginInCurrentTab(tab);});
+function showPageActionInNetsuiteTab(tabId, selectInfo)
+{
+    chrome.tabs.get(tabId, function(tab)
+    {
+        var loginPageRegex = /https:\/\/.*netsuite\.com\/pages\/login\.jsp\?rdt=.*/;
+        if(loginPageRegex.test(tab.url))
+        {
+            chrome.pageAction.show(tabId);
+        }
+    });
+}
+
+chrome.pageAction.onClicked.addListener(function (tab) {skipNetSuiteLoginInCurrentTab(tab);});
+chrome.tabs.onSelectionChanged.addListener(showPageActionInNetsuiteTab);
